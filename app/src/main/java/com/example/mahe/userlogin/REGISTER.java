7244d5fef868252090056b0880cfa9be6640bf78
Ashish <ashishsamanta007@gmail.com>
2017-05-29 11:02:01 +0530

    USERLOGIN

---
+++ app/src/main/java/com/example/mahe/userlogin/REGISTER.java
@@ -0,0 +1,24 @@
+package com.example.mahe.userlogin;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.content.Intent;
+import android.view.View;
+import android.widget.EditText;
+
+public class REGISTER extends AppCompatActivity {
+
+
+
+    public void onClick(View view)
+    {
+        Intent i = new Intent(this,MainActivity.class);
+        startActivity(i);
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+    }
+}
